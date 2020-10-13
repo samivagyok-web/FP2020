fa94d5bc6043b1e8880eb9653241efbe9a0909c5
samivagyok-web <samivagyok@gmail.com>
2020-10-13 10:28:01 +0300

    added triunghi checker+reverse number

---
+++ 2_1310/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar sub 999: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double lenghtOfInt = Math.Floor(Math.Log10(n)) + 1;
+
+            switch (lenghtOfInt)
+            {
+                case 1:
+                    Console.Write(n);
+                    break;
+                case 2:
+                    Console.Write(n % 10);
+                    Console.Write(n / 10);
+                    break;
+                case 3:
+                    Console.Write(n % 10);
+                    Console.Write((n / 10) % 10);
+                    Console.Write(n / 100);
+                    break;
+                default:
+                    Console.WriteLine("Nu ai dat numar sub 999.");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ 2_1310/Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1310
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti 3 numere care vor fi latura triunghului: ");
+            double a, b, c;
+
+            Console.Write("a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("c: ");
+            c = int.Parse(Console.ReadLine());
+
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine("SE POATE TRIUNGHI");
+            }
+            else Console.WriteLine("not good");
+        }
+    }
+}
